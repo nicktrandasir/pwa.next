79519ac6c5b4120327abf315ec3a9feff454f4e8
nick.trandasir <nick.trandasir@gmail.com>
2022-09-28 14:27:42 +0300

    PWA02 - Config pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
+
 export default function Document() {
     return (
         <Html>
             <Head>
                 <link rel="manifest" href="/manifest.json" />
-                <link rel="apple-touch-icon" href="/icon.png"></link>
+                <link rel="icon-192x192" href="/icon-192x192.png"></link>
                 <meta name="theme-color" content="#fff" />
+
+
             </Head>
             <body>
             <Main />
